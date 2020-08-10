30ca87368f79045707bed7990d9c8971e269b31d
AlaKlein <alaeklein@gmail.com>
2020-08-10 20:59:59 -0300

    First Commit.

--- src/main/java/br/univates/gerformhibernate/main.java
+++ src/main/java/br/univates/gerformhibernate/main.java
@@ -15,7 +15,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("relou");
+        System.out.println("relou mundo");
     }
     
 }
