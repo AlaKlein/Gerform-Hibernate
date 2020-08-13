4264b4949e702a675c80d9b85be73ce4c1f870d8
AlaKlein <alaeklein@gmail.com>
2020-08-13 09:51:03 -0300

    Reolved issues with hibernate-core and cfg.xml file. Fist Insert succeded.

--- src/main/java/gerform/main.java
+++ src/main/java/gerform/main.java
@@ -5,7 +5,13 @@
  */
 package gerform;
 
+import Entidade.Usuario;
 import Tela.IfrUsuario;
+import javax.swing.JOptionPane;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
 
 /**
  *
@@ -17,8 +23,35 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        IfrUsuario tela = new IfrUsuario();
-        tela.setVisible(true);
-    }
+        /*Session session = HibernateFiles.HibernateUtil.getSessionFactory().openSession();
 
+        session.beginTransaction();
+        Usuario user = new Usuario();
+
+        user.setId(1);
+        user.setEmail("Mukesh");
+        user.setSenha("Google");
+
+        session.save(user);
+        session.getTransaction().commit();*/
+
+        Session sessao = null;
+        try {
+            sessao = HibernateFiles.HibernateUtil.getSessionFactory().openSession();
+            Transaction transacao = sessao.beginTransaction();
+            Usuario user = new Usuario();
+            //user.setId(5);
+            user.setEmail("jose");
+            user.setPermissao("Administrador");
+            user.setSenha("1234");
+
+            sessao.save(user);
+            transacao.commit();
+            JOptionPane.showMessageDialog(null, "Cadastro efetuado com sucesso!");
+        } catch (HibernateException hibEx) {
+            hibEx.printStackTrace();
+        } finally {
+            sessao.close();
+        }
+    }
 }
