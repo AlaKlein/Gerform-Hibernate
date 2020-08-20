d0cd97aedc57b8084620a79beb9f05702aff2063
samuelwerner98 <samuel.werner@universo.univates.br>
2020-08-19 21:54:37 -0300

    PropriedadesMaterial, there are a lot of errors

---
+++ src/main/java/Entidade/PropriedadesMaterial.java
@@ -0,0 +1,94 @@
+package Entidade;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "propriedades_material")
+
+public class PropriedadesMaterial implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "usuario_id")
+    private int usuario_id;
+    
+    @Column(name = "material_id")
+    private int material_id;
+    
+    @Column(name = "umidade")
+    private double umidade;
+    
+    @Column(name = "gordura")
+    private double gordura;
+    
+    @Column(name = "proteina")
+    private double proteina;
+    
+    @Column(name = "status")
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(int usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public int getMaterial_id() {
+        return material_id;
+    }
+
+    public void setMaterial_id(int material_id) {
+        this.material_id = material_id;
+    }
+
+    public double getUmidade() {
+        return umidade;
+    }
+
+    public void setUmidade(double umidade) {
+        this.umidade = umidade;
+    }
+
+    public double getGordura() {
+        return gordura;
+    }
+
+    public void setGordura(double gordura) {
+        this.gordura = gordura;
+    }
+
+    public double getProteina() {
+        return proteina;
+    }
+
+    public void setProteina(double proteina) {
+        this.proteina = proteina;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
