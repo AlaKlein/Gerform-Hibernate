efaa9eaddb39e3c0777f67c7e8059bd80db40336
AlaKlein <alaeklein@gmail.com>
2020-12-03 23:13:53 -0300

    fixed audit wrong name.

--- src/main/java/Dao/FormulacaoDAO.java
+++ src/main/java/Dao/FormulacaoDAO.java
@@ -125,7 +125,7 @@ public class FormulacaoDAO implements IDAO_T<Formulacao> {
             transacao.commit();
 
             if (TelaPrincipal.ligaAuditoria) {
-                Audita.salvarAuditoria("Update", "Formulacao", UsuarioLogado.getUsuarioLogadoID());
+                Audita.salvarAuditoria("Update", "formulacao", UsuarioLogado.getUsuarioLogadoID());
             }
             
         } catch (HibernateException hibEx) {
